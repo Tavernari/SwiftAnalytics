c0b8786cc4bb2224d94b25156e03ea2320b5d278
Victor C Tavernari <victortavernari@gmail.com>
2020-02-21 09:35:45 -0300

    fix syntax UIApplication.LaunchOptionsKey

--- Example/SwiftAnalytics/AppDelegate.swift
+++ Example/SwiftAnalytics/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         SwiftAnalytics.registerBroker(name: "AnalyticsService") { (name, params) -> Bool in
             FakeAnalytics.call(name: name, params: params)
