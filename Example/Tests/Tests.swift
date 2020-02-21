ddcaf27aa9e0d6ff707c42cde6a166cce8898413
Victor C Tavernari <victortavernari@gmail.com>
2020-02-21 09:22:49 -0300

    remove nimble from pod

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,6 +1,5 @@
 import XCTest
 import SwiftAnalytics
-import Nimble
 
 class Tests: XCTestCase {
 
@@ -15,10 +14,10 @@ class Tests: XCTestCase {
         let eventName1 = "eventName1"
         let eventName2 = "eventName2"
         SwiftAnalytics.registerBroker(name: broker1) { (name, _) -> Bool in
-            expect(name).to(equal(eventName1))
+            XCTAssertEqual(name, eventName1)
             SwiftAnalytics.unregisterBroker(name: broker1)
             SwiftAnalytics.registerBroker(name: broker2) { (name, _) -> Bool in
-                expect(name).to(equal(eventName2))
+                XCTAssertEqual(name, eventName2)
                 return true
             }
             return true
